c51040b56d9957769e8676f78f66c3a211945033
ivantrapp <ivangabrieltrapp@gmail.com>
2025-06-27 00:51:47 +0000

    DIA 26/06

--- app-a/src/main/java/br/univille/app_a/HomeController.java
+++ app-a/src/main/java/br/univille/app_a/HomeController.java
@@ -1,14 +1,14 @@
-package br.univille.app_a.controller;
+package br.univille.app_a;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.dapr.client.DaprClient;
 import io.dapr.client.DaprClientBuilder;
 import io.dapr.client.domain.HttpExtension;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api")
@@ -18,7 +18,7 @@ public class HomeController {
 
     @GetMapping()
     public ResponseEntity index() {
-
+        
         try(DaprClient daprClient = new DaprClientBuilder().build()){
             System.out.println("Hello from b");
             daprClient.invokeMethod("app-b", "/api", null, HttpExtension.GET).block();            
@@ -26,7 +26,21 @@ public class HomeController {
             // TODO: handle exception
         }
 
-
         return ResponseEntity.ok().body("Hello from App A");
+    }
+
+    @PostMapping("/startaasync")
+    public ResponseEntity startAAsync(){
+        System.out.println("App A started");
+        
+        try(DaprClient daprClient = new DaprClientBuilder().build()){
+            var message = "Bombástica fabrica de mensagem";
+            System.out.println("Rodou AA");
+            daprClient.publishEvent("pubsub-dapr", "topicdapr", message).block();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok().body("App A started");
     }
 }
--- app-b/src/main/java/br/univille/app_b/controller/HomeController.java
+++ app-b/src/main/java/br/univille/app_b/controller/HomeController.java
@@ -1,9 +1,15 @@
 package br.univille.app_b.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.dapr.Topic;
+import io.dapr.client.domain.CloudEvent;
 
 @RestController
 @RequestMapping("/api")
@@ -14,5 +20,19 @@ public class HomeController {
     @GetMapping()
     public ResponseEntity index() {
         return ResponseEntity.ok().body("Hello from App B");
+    }
+
+    @Topic(pubsubName="pubsub-dapr", name="topicdapr")
+    @PostMapping(path = "/startbasync", consumes = MediaType.ALL_VALUE)
+    public ResponseEntity startBAsync(@RequestBody(required= false) CloudEvent<String> evento){
+
+        System.out.println("App B started");
+
+        var id = evento.getId();
+        var mensagem = evento.getData();
+
+        System.out.println("Mensagem" + mensagem);
+        System.out.println("id" + id);
+        return ResponseEntity.ok().body("body");
     }
 }
